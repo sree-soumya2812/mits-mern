5463ff7d048aa317e172b2aa0fa52f9a0c1d122c
sree-soumya2812 <sreesoumyagollapinni@gmail.com>
2026-01-22 11:36:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+var a = 10;//global scope
+console.log(a);
+let b = 20;//block scope
+console.log(b);
+const c = 30;
+console.log(c);//constant value
+var d = 12;
+var str = "soumya";
+var bool = true;
+var und;
+var nu = null;
+var big = 12345678901234567890n;
+console.log(typeof a);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof Symbol);
