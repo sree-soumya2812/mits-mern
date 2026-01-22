49dedbb8783b0b7df787a6ed8aee112b1768f3b5
sree-soumya2812 <sreesoumyagollapinni@gmail.com>
2026-01-22 11:48:27 +0530

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,6 @@
+a = true;
+b = false;
+console.log(a && b);//true if both are true
+console.log(a || b);//true if one is true
+console.log(!a);//true if a is false
+console.log(!b);//true if b is false
