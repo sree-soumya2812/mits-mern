cee5585933dba4bca6799820e0770d6c8836fa19
sree-soumya2812 <sreesoumyagollapinni@gmail.com>
2026-01-22 11:43:48 +0530

    Add files via upload

---
+++ relational.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = "10";
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);    
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
